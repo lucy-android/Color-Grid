231900d729e0dfeccb9b7fd4da93ff1fb672c04e
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2025-04-20 18:18:52 +0300

    COL-2: hide status bar

--- composeApp/src/androidMain/kotlin/compose/project/demo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/compose/project/demo/MainActivity.kt
@@ -3,16 +3,31 @@ package compose.project.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+            insetsController.apply {
+                hide(WindowInsetsCompat.Type.statusBars())
+                hide(WindowInsetsCompat.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+            }
+            MaterialTheme {
+                App()
+            }
         }
+        enableEdgeToEdge()
     }
 }
 
