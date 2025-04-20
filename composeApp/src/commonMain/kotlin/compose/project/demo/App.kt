60cd3813892af099dec499e0acdcd539e92488e4
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2025-04-20 18:03:35 +0300

    create color grid

--- composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -1,67 +1,131 @@
 package compose.project.demo
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.platform.InspectorValueInfo
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.unit.Constraints
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
+
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var location by remember { mutableStateOf("Europe/Paris") }
-        var timeAtLocation by remember { mutableStateOf("No location selected") }
+        GridLayout()
+    }
+}
 
 
-        Column(modifier = Modifier.padding(20.dp)) {
-            Text(
-                text = timeAtLocation,
-                style = TextStyle(fontSize = 20.sp),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
+@Composable
+fun GridLayout(
+    modifier: Modifier = Modifier
+) {
+    val numbers = (0..20).toList()
+
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(4)
+    ) {
+        items(numbers.size) {
+            val color = Color(
+                Random.nextInt(255),
+                Random.nextInt(255),
+                Random.nextInt(255),
+                Random.nextInt(255),
             )
-            TextField(
-                value = location,
-                modifier = Modifier.padding(top = 10.dp),
-                onValueChange = { location = it })
-            Button(
-                modifier = Modifier.padding(top = 10.dp),
-                onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid location" }) {
-                Text("Show Time")
+            Box(modifier = Modifier
+                .background(color)
+                .squareSize()) {
             }
         }
     }
 }
 
-fun currentTimeAt(location: String): String? {
-    fun LocalTime.formatted() = "$hour:$minute:$second"
 
-    return try {
-        val time = Clock.System.now()
-        val zone = TimeZone.of(location)
-        val localTime = time.toLocalDateTime(zone).time
-        "The time in $location is ${localTime.formatted()}"
-    } catch (ex: IllegalArgumentException) {
-        null
+@Stable
+fun Modifier.squareSize(
+    position: Float = 0.5f,
+): Modifier =
+    this.then(
+        when {
+            position == 0.5f -> SquareSizeCenter
+            else -> createSquareSizeModifier(position = position)
+        }
+    )
+
+private val SquareSizeCenter = createSquareSizeModifier(position = 0.5f)
+
+private class SquareSizeModifier(
+    private val position: Float,
+    inspectorInfo: InspectorInfo.() -> Unit,
+) : LayoutModifier, InspectorValueInfo(inspectorInfo) {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints,
+    ): MeasureResult {
+        val maxSquare = min(constraints.maxWidth, constraints.maxHeight)
+        val minSquare = max(constraints.minWidth, constraints.minHeight)
+        val squareExists = (minSquare <= maxSquare)
+
+        val resolvedConstraints = constraints
+            .takeUnless { squareExists }
+            ?: constraints.copy(maxWidth = maxSquare, maxHeight = maxSquare)
+
+        val placeable = measurable.measure(resolvedConstraints)
+
+        return if (squareExists) {
+            val size = max(placeable.width, placeable.height)
+            layout(size, size) {
+                val x = ((size - placeable.width) * position).toInt()
+                val y = ((size - placeable.height) * position).toInt()
+                placeable.placeRelative(x, y)
+            }
+        } else {
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(0, 0)
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is SquareSizeModifier) return false
+
+        if (position != other.position) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return position.hashCode()
     }
 }
 
+@Suppress("ModifierFactoryExtensionFunction", "ModifierFactoryReturnType")
+private fun createSquareSizeModifier(
+    position: Float,
+) =
+    SquareSizeModifier(
+        position = position,
+        inspectorInfo = debugInspectorInfo {
+            name = "squareSize"
+            properties["position"] = position
+        },
+    )
